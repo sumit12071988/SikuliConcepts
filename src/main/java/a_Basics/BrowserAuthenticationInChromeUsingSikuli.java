dabb6c0e6572b8c2a47d99cc61ad83781675c2b2
sumit12071988 <sumit786.88@gmail.com>
2022-07-04 13:34:28 +0800

    Changed screenshots

--- src/main/java/a_Basics/BrowserAuthenticationInChromeUsingSikuli.java
+++ src/main/java/a_Basics/BrowserAuthenticationInChromeUsingSikuli.java
@@ -5,13 +5,31 @@ package a_Basics;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.sikuli.hotkey.Keys;
 import org.sikuli.script.Pattern;
 import org.sikuli.script.Screen;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BrowserAuthenticationInChromeUsingSikuli {
+
+	WebDriver driver;
+
+	@BeforeMethod
+	public void setup(){
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://the-internet.herokuapp.com/basic_auth");
+	}
+
+	@AfterMethod
+	public void teardown(){
+		driver.close();
+	}
 	
 	@Test
 	public void test() throws Exception {
@@ -20,15 +38,8 @@ public class BrowserAuthenticationInChromeUsingSikuli {
 		Pattern txt_AuthPopUp_UserName = new Pattern(System.getProperty("user.dir")+"\\SikuliImageLibrary\\txt_AuthPopUp_UserName.PNG");
 		Pattern txt_AuthPopUp_Password = new Pattern(System.getProperty("user.dir")+"\\SikuliImageLibrary\\txt_AuthPopUp_Password.PNG");
 		Pattern btn_AuthPopUp_SignIn = new Pattern(System.getProperty("user.dir")+"\\SikuliImageLibrary\\btn_AuthPopUp_SignIn.PNG");
-		
-			
-		//*********** DRIVER ACTIONS ************
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://the-internet.herokuapp.com/basic_auth");
-		
 
-		
+
 		// *********** SIKULI ACTIONS *************
 		// Create an object of Screen class
 		//	Note: Don't use wait in ALL type b_Actions if multiple textboxes are present in same window. Use the action directly.
@@ -36,16 +47,14 @@ public class BrowserAuthenticationInChromeUsingSikuli {
 		
 		// Step4: Use Sikuli scripts
 		s.wait(txt_AuthPopUp_UserName,2).type("admin");
-		
+		s.type(Keys.TAB);
+		s.wait(txt_AuthPopUp_Password,2).type("admin");
+
 		// Sometimes wait will not bring the cursor to the element in which we need to type. In such cases we should use action directly.
-		s.type(txt_AuthPopUp_Password,"admin");
+//		s.type(txt_AuthPopUp_Password,"admin");
 		//screen.wait(txt_AuthPopUp_Password,2).type("admin");
 	
 		s.wait(btn_AuthPopUp_SignIn,2).click();
-		
-	
-		driver.close();
-		
 	}
 
 }
